b362057b2251b22b3be45779c31ce356fe54c252
Alex Imenwo <aleximenwo@gmail.com>
2021-06-15 19:00:22 +0100

    cors issues

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['http://www.wager.com.ng', 'www.wager.com.ng', 'http://www.wager.com.ng', 'http://localhost:4200'],
+    origin: ['http://wager.com.ng', 'www.wager.com.ng', 'http://www.wager.com.ng', 'http://localhost:4200'],
     credentials: true
 }));
 
